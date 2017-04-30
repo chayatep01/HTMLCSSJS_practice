3090be7f74e83c78d89587a6e86e44a0a119d01c
chayatep <chayatep01@hotmail.com>
2017-05-01 01:22:36 +0700

    mycode
    My first commited on GitHub

---
+++ firstScript.js
@@ -0,0 +1,31 @@
+ 
+            //interactByOnclick
+            
+            document.getElementById("buttonOutput").onclick = function(){
+                document.getElementById("OutputformClick").innerHTML = " you have been clicked ";
+                
+            }
+
+            //JSifstatement
+            document.getElementById("Xentered").onclick = function(){
+                var x  = document.getElementById("inputX").value ;
+                if(x==0){
+
+                    alert("X equal to zero");
+                    
+
+                }else{
+                    alert("X not equal to zero");
+                   
+                }
+                
+            }
+            
+          
+            
+            
+            
+                
+                
+
+
